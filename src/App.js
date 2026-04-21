bac030d875f5670002dfe1ff5d8d3a72e3a71149
MBMB Deployment <deploy@mbmb.com>
2026-04-21 13:53:00 -0400

    Initial deployment - AIMS Medical Assessment Platform with Supabase integration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AIMS Medical Assessment Platform</h1>
+        <p>MSK Assessment with Anti-Hallucination RAG</p>
+        <p>Connected to Supabase: vodhhauwowkalvaxzqyv</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
